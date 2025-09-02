0b355b65f5ecb28823f9283f7be00cd7a66bf88b
brunocignachi-cloud <bruno.cignachi@aluno.farroupilha.ifrs.edu.br>
2025-09-02 20:00:03 -0300

    Delete Trabalho Dev. Web directory

--- Trabalho Dev. Web/src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-
-mongoose.connect("mongodb+srv://volpinivini:zyBdjK5PThWa9plv@mycluster.oe7rhah.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster");
-
-let db = mongoose.connection;
-
-export default db;
